9c0cdd367ab83aaea3158454308cdd443d9657ec
Jeff <jeffosten@gmail.com>
2019-02-07 11:03:28 +0100

    Updates

---
+++ precache-manifest.f6aefd2d667d3b5bec1827a2dbcbd6f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd4a608f0493103606934a01ce54aef9",
+    "url": "/convertcurrencies/static/media/change.bd4a608f.png"
+  },
+  {
+    "revision": "94f079ea1bc9507fba3e",
+    "url": "/convertcurrencies/static/js/runtime~main.94f079ea.js"
+  },
+  {
+    "revision": "b3b37b793df3a9bc4527",
+    "url": "/convertcurrencies/static/js/main.b3b37b79.chunk.js"
+  },
+  {
+    "revision": "6b6649210ea720f3c26f",
+    "url": "/convertcurrencies/static/js/1.6b664921.chunk.js"
+  },
+  {
+    "revision": "b3b37b793df3a9bc4527",
+    "url": "/convertcurrencies/static/css/main.80964a7d.chunk.css"
+  },
+  {
+    "revision": "2d4b5f1566ee66575f34de289bc7e78e",
+    "url": "/convertcurrencies/index.html"
+  }
+];
